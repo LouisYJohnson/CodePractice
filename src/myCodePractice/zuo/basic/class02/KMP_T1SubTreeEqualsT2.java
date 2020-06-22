e52ebb09da1e664a3541bd7907f3ed92e1283b0e
LouisYJohnson <623988524@qq.com>
2020-06-22 15:26:34 +0800

    bug fixes and code practice

---
+++ src/myCodePractice/zuo/basic/class02/KMP_T1SubTreeEqualsT2.java
@@ -0,0 +1,14 @@
+package myCodePractice.zuo.basic.class02;
+
+public class KMP_T1SubTreeEqualsT2 {
+    //给定两个二叉树T1和T2， 返回T1的某个子树结构是否与T2的结构相等。
+    //将两个字符串以前中后序(自选)遍历并打印成字符串,看T2对应的字符串是否为T1的子串
+
+    //实现使用Morris遍历+KMP
+    //实现Morris遍历
+    public static String morrisPre() {
+
+        return null;
+    }
+
+}
